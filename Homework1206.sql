2ebc643a2c2db026acff9d9a29375307701e8cf0
notelly <117340086+notelly@users.noreply.github.com>
2022-12-06 11:23:06 +0900

    Update Homework1206.sql

--- Homework1206.sql
+++ Homework1206.sql
@@ -109,3 +109,16 @@ FROM employees;
 --SA_REP D
 --ST_CLERK E
 --None of the above 0
+
+
+SELECT job_id,
+       CASE job_id WHEN 'AD_PRES' THEN  'A'
+                   WHEN 'ST_MAN' THEN  'B'
+                   WHEN 'IT_PROG' THEN  'C'
+                   WHEN 'SA_REP' THEN  'D'
+                   WHEN 'ST_CLERK' THEN  'E'
+                   ELSE 'None of the above 0'
+       END
+       AS employee_grade
+FROM employees;
+
